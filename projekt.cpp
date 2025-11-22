129ef4150a2f6bc722c6d0e1d0bbe6656b1b88a7
blajak <blazej.0411@wp.pl>
2025-11-22 18:51:31 +0000

    czy_dodatnia

--- projekt.cpp
+++ projekt.cpp
@@ -5,6 +5,14 @@ int main()
   {
     int x;
     cin >> x;
+    
+    if(x <= 0)
+      {
+        cout << "Podana liczba nie jest dodatnia" << endl;
+        return 0;
+      }
+    
+    
     cout << x << endl;
     return 0;
   }
