74ce55616c4e518581125358cf1c9859e50c7258
blajak <blazej.0411@wp.pl>
2025-11-22 18:48:53 +0000

    czy_pierwsza

--- projekt.cpp
+++ projekt.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
+ bool czy_pierwsza(int n) {
+    if (n < 2) return false;
+    for (int i = 2;i*i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+  
+  
 int main()
   {
     int x;
     cin >> x;
-    cout << x << endl;
+    cout << x << " ";
+    if(czy_pierwsza(x))
+      cout << "to jest liczba pierwsza";
+    else
+      cout << "to nie jest liczba pierwsza";
+      
+    cout << endl;
     return 0;
   }
